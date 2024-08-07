33deb6838630bbb20a88cb36674477e35276640e
W1zZo <paolo_maggiore@hotmail.it>
2024-08-07 17:18:53 +0200

    Fixed spell levels

--- src/module/sheets/actor-sheet.mjs
+++ src/module/sheets/actor-sheet.mjs
@@ -106,7 +106,6 @@ export class DeckOfDestinyActorSheet extends ActorSheet {
     const gear = [];
     const features = [];
     const spells = {
-      0: [],
       1: [],
       2: [],
       3: [],
